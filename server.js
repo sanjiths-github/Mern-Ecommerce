b28542379c6107b29bb7cdae1f7867cc7f6e08af
Sanjith S <130857908+sanjiths-github@users.noreply.github.com>
2023-10-07 16:11:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/ProductRoutes.js"
 import cors from 'cors'
-
+import path from 'path'
 dotenv.config()
 
 connectDB()
@@ -18,6 +18,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 
 //routes
@@ -25,8 +26,8 @@ app.use("/api/v1/auth",authRoutes)
 app.use('/api/v1/category',categoryRoutes)
 app.use('/api/v1/product',productRoutes)
 
-app.get('/',(req,res) => {
-    res.send("<h1>welcome<h1>")
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
 })
 
 const PORT = process.env.PORT || 8080
